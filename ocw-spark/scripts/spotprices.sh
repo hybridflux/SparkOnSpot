22d51d0b7b9b358b87c77dbc4fc46f49838b0229
Sujit D'Mello <sujit@hotmail.com>
2021-07-16 15:21:28 -0400

    Fixed formatting

--- ocw-spark/scripts/spotprices.sh
+++ ocw-spark/scripts/spotprices.sh
@@ -1,9 +1,9 @@
-az account list-locations -o table --query "[].name" | sed 1d | \
+az account list-locations -o tsv --query "[].name" | sed 1d | \
 while read i
     do
-    echo $i'\c'
-    j=$(az rest --method GET --url "https://prices.azure.com/api/retail/prices?\$filter=contains(skuName,'DS3 v2 Spot') AND armRegionName eq '$i' AND productName eq 'Virtual Machines DSv2 Series'" --query 'Items [0].unitPrice' --only-show-errors)
-    echo ","$j'\c'
-    k=$(az rest --method GET --url "https://prices.azure.com/api/retail/prices?\$filter=skuName eq 'DS3 v2' AND armRegionName eq '$i' AND productName eq 'Virtual Machines DSv2 Series' AND reservationTerm eq null" --query 'Items [0].unitPrice' --only-show-errors)
+    echo -n $i
+    j=$(az rest --method GET --url "https://prices.azure.com/api/retail/prices?\$filter=contains(skuName,'DS3 v2 Spot') AND armRegionName eq '$i' AND productName eq 'Virtual Machines DSv2 Series'" --query 'Items [0].unitPrice' --only-show-errors -o tsv)
+    echo -n ","$j
+    k=$(az rest --method GET --url "https://prices.azure.com/api/retail/prices?\$filter=skuName eq 'DS3 v2' AND armRegionName eq '$i' AND productName eq 'Virtual Machines DSv2 Series' AND reservationTerm eq null" --query 'Items [0].unitPrice' --only-show-errors -o tsv)
     echo ","$k
     done
